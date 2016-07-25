31a66c4e2e7bb832227708c5467e1da43d9f3263
MRKjeld <91025951@mail.computerpower.ac.nz>
2016-07-26 11:45:52 +1200

    Update SoT.java

--- SoT.java
+++ SoT.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Hackfest;
 
-/**
- *
- * @author 91025951
- */
 public class SoT {
     
     static String w = "wording";
