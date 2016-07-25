8e9c5d8c7f074e9b84d83bfff76e7db417da2743
MRKjeld <91025951@mail.computerpower.ac.nz>
2016-07-26 11:46:32 +1200

    Update FourLettersInCommon.java

--- FourLettersInCommon.java
+++ FourLettersInCommon.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Hackfest;
 
 import java.util.*;
@@ -10,16 +5,18 @@ import java.util.*;
 public class FourLettersInCommon {
         
     public List<String> fourLettersInCommon(String word, String[] dictionary){
+        //stores an ArrayList of sample words consisting of 4+ letters from word.
         List<String> sampleWords= new ArrayList<String>();
+        //stores an ArrayList of dictionary words that contain 4+ letters from sample word.
         List<String> lettersInCommon = new ArrayList<String>();
         
-        //will contain a dictionary of sample words to run up against dictionary.
-        
+        //Fills sampleWords ArrayList with possible combinations.
         for(int startIndex = 0; startIndex<=word.length();startIndex++){
             for(int endIndex = startIndex+4; endIndex<=word.length();endIndex++){
                 sampleWords.add(word.substring(startIndex,endIndex));
             }
         }
+        //Cycles through dictionary strings to check word by word if they contain the letters from sampleWords array
         for(int dCycle = 0; dCycle<dictionary.length;dCycle++){
             for(int wCycle = 0; wCycle<sampleWords.size();wCycle++){
                 if(dictionary[dCycle].contains(sampleWords.get(wCycle))){
